7901ef1b64e2c41798f107487bea83ad1201f263
Lucas Oliveira <lu.oliveira.silva98@gmail.com>
2022-12-18 14:07:51 +0000

    validating a number range by annotation added to project

--- WebContent/WEB-INF/view/customerForm.jsp
+++ WebContent/WEB-INF/view/customerForm.jsp
@@ -19,6 +19,9 @@
 	 	Last Name*: <form:input path="lastName"/>
 	 	<form:errors path="lastName" cssClass="error"/>
 	 	<br><br>
+	 	Free Passes: <form:input path="freePasses"/>
+	 	<form:errors path="freePasses" cssClass="error"/>
+	 	<br><br>
 	 	<input type="submit" value="Submit" />
 	</form:form>
 </body>
--- src/br/pessoal/springDemo/mvc/Customer.java
+++ src/br/pessoal/springDemo/mvc/Customer.java
@@ -1,5 +1,7 @@
 package br.pessoal.springDemo.mvc;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -11,7 +13,18 @@ public class Customer {
 	@Size(min = 3, message = "too short")
 	private String lastName;
 	
+	@Min(value = 0, message = "must be greater than 0 or equal to zero")
+	@Max(value = 10, message = "must be less than or equal to ten")
+	private int freePasses;
 	
+	
+	
+	public int getFreePasses() {
+		return freePasses;
+	}
+	public void setFreePasses(int freePasses) {
+		this.freePasses = freePasses;
+	}
 	public String getFirstName() {
 		return firstName;
 	}
